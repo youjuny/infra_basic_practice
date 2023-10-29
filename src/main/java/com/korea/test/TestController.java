31e71d1f9f5a506169a07d9d29a182bfc23496b8
chacha86 <taejincha0619@gmail.com>
2023-10-29 14:21:54 +0900

    필수미션 1 해결

--- src/main/java/com/korea/test/TestController.java
+++ src/main/java/com/korea/test/TestController.java
@@ -24,6 +24,11 @@ public class TestController {
         //1. DB에서 데이터 꺼내오기
         List<Post> postList = postRepository.findAll();
 
+        if(postList.isEmpty()) {
+            saveDefault();
+            return "redirect:/";
+        }
+
         //2. 꺼내온 데이터를 템플릿으로 보내기
         model.addAttribute("postList", postList);
         model.addAttribute("targetPost", postList.get(0));
@@ -33,13 +38,7 @@ public class TestController {
 
     @PostMapping("/write")
     public String write() {
-        Post post = new Post();
-        post.setTitle("new title..");
-        post.setContent("");
-        post.setCreateDate(LocalDateTime.now());
-
-        postRepository.save(post);
-
+        saveDefault();
         return "redirect:/";
     }
 
@@ -59,5 +58,14 @@ public class TestController {
 
         postRepository.save(post);
         return "redirect:/detail/" + id;
+    }
+
+    private void saveDefault() {
+        Post post = new Post();
+        post.setTitle("new title..");
+        post.setContent("");
+        post.setCreateDate(LocalDateTime.now());
+
+        postRepository.save(post);
     }
 }
